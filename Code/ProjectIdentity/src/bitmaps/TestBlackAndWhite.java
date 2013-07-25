631c178562b2a56ef032a534746c6cd13314cade
MSaldanaNox <msaldana@student.neumont.edu>
2013-07-25 14:20:32 -0600

    Working revision, new pictures for sample data
    Revert back to before pass off if task is not complete

--- Code/ProjectIdentity/src/bitmaps/TestBlackAndWhite.java
+++ Code/ProjectIdentity/src/bitmaps/TestBlackAndWhite.java
@@ -4,11 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorConvertOp;
-import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,9 +17,7 @@ import javax.swing.UIManager;
 
 public class TestBlackAndWhite {
 
-	int rgbX, rgbY;
 	List<String> coords;
-	
 	
 	public static void main(String[] args) {
 		new TestBlackAndWhite();
@@ -53,49 +47,15 @@ public class TestBlackAndWhite {
 	public class TestPane extends JPanel {
 
 		private BufferedImage master;
-		private BufferedImage depthMap;
-		private BufferedImage grayScale;
-		private BufferedImage blackWhite;
 
 		public TestPane() {
 			try {
-				master = ImageIO.read(new File("./images/WP_001078.jpg"));
-				blackWhite = ImageIO.read(new File("./images/WP_001078.jpg"));
-				grayScale = ImageIO.read(new File("./images/WP_001078.jpg"));
-				ColorConvertOp op = new ColorConvertOp(
-						ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
-				op.filter(grayScale, grayScale);
-
-				if (grayScale == null) {
-					File outputfile = new File("./images/saved.png");
-					ImageIO.write(grayScale, "png", outputfile);
-				} else {
-					blackWhite = new BufferedImage(master.getWidth(),
-							master.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-					Graphics2D g2d = blackWhite.createGraphics();
-
-					g2d.drawImage(grayScale, 0, 0, (ImageObserver) this);
-					
-					g2d.dispose();
-				}
+				master = ImageIO.read(new File("./images/test5.jpg"));
 				
 				coords = new ArrayList<String>();
-
-				SkinTones st = new SkinTones();
 				
-				for (int h = 0; h < blackWhite.getWidth(); h++) {
-					for (int v = 0; v < blackWhite.getHeight(); v++) {
-						
-//						boolean isIn = false;
-						
-//						for(Color c : st.getSkinTones())
-//						{
-//							Color e = new Color(master.getRGB(h, v));
-//							if(c.getRGB()==e.getRGB())
-//							{
-//								isIn = true;
-//							}
-//						}
+				for (int h = 0; h < master.getWidth(); h++) {
+					for (int v = 0; v < master.getHeight(); v++) {
 						Color e = new Color(master.getRGB(h, v));
 						if(isSkinRGB(e.getRed(), e.getBlue(), e.getRed()))
 						{
@@ -126,10 +86,12 @@ public class TestBlackAndWhite {
 				int x = (getWidth() - (master.getWidth() * 2)) / 2;
 				int y = (getHeight() - master.getHeight()) / 2;
 				
+				// Draw original image
 				g.drawImage(master, x, y, this);
+				
+				// Draw image with identified skin-toned pixels
 				x += master.getWidth();
 				g.drawImage(master, x, y, this);
-
 				for(String coord : coords)
 				{
 					String[] toPaint = coord.split(" ");
